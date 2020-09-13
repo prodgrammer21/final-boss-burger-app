1e1f2d7ca4c5d62d07253000450024b4aed72da1
Rodrick <rodrick.alcantara@gmail.com>
2020-09-13 16:59:38 +0800

    Add mysql

--- server.js
+++ server.js
@@ -21,6 +21,19 @@ app.use("/", appRoute);
 
 const PORT = process.env.PORT || 8080;
 
+const mysql = require("mysql");
+const pool = mysql.createPool({
+  host: "us-cdbr-east-02.cleardb.com",
+  user: "b7c212e08dd222",
+  password: "a01ec5c3",
+  database: "heroku_24fe9c09a6dc6ce",
+});
+
+pool.query("SELECT 1 + 1 AS solution", function (error, results, fields) {
+  if (error) throw error;
+  console.log("The solution is: ", results[0].solution);
+});
+
 app.listen(PORT, () => {
   console.log(`Listening on port: ${PORT}`);
 });
